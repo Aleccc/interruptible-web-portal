5eead4699c31222baa95658fe04fba506921840d
Alec <alec.kaye@gmail.com>
2019-10-31 04:07:16 -0400

    successfully logout

--- settings/urls.py
+++ settings/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='login.html'),
          name='login'),
-    # path('logout/', LogoutView.as_view(next_page=reverse_lazy('index')),
-    #      name='logout'),
+    path('logout/', LogoutView.as_view(next_page='/login'),
+         name='logout'),
 ]
