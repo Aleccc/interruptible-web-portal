044f5f936e8c4792797246e504f4b5837ba3fdcf
Aleccc <alec.kaye@gmail.com>
2019-11-05 16:26:52 -0500

    Update settings.py

--- settings/settings.py
+++ settings/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = 'index'
