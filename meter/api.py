201b99050f42419a91a599c92c4d53a0ee7d1dab
alec <alec.kaye@gmail.com>
2019-11-06 15:14:20 -0500

    add post and put for meter

--- meter/api.py
+++ meter/api.py
@@ -1,8 +1,12 @@
 from tastypie.resources import ModelResource
+from tastypie.fields import ToManyField
 from .models import Meter
 
 
 class MeterResource(ModelResource):
+    meter_read = ToManyField('meter_read.api.MeterReadResource',
+                             'meter_read', full=True, null=True, blank=True)
+
     class Meta:
         queryset = Meter.objects.all()
-        allowed_methods = ['get']
+        allowed_methods = ['get', 'post', 'put']
